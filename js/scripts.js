65062df745f26bd1a0ca57787ae2dda613be30d3
Cecilia Le Brech <cecilialebrch@gmail.com>
2016-07-12 00:17:33 -0700

    change js

--- js/scripts.js
+++ js/scripts.js
@@ -4,34 +4,58 @@ function Player(score, turn) {
   this.turn = turn;
 }
 
-
-var rolledNumber =function(){
+//for player1//
+var rolledNumber1 =function(){
   return Math.floor((Math.random() * (6)) + 1);
 };
 
-var currentScore= 0;
+var currentScore1= 0;
 
 
-var roll= function() {
-  var randomNumber= rolledNumber();
-  alert( randomNumber);
-  if (randomNumber === 1){
-    currentScore = 0;
+var roll1= function() {
+  var randomNumber1= rolledNumber1();
+  alert( randomNumber1);
+  if (randomNumber1 === 1){
+    currentScore1 = 0;
   }else {
-    currentScore += randomNumber;
+    currentScore1 += randomNumber1;
   }
 
 }
 
 
-// var hold= function(currentScore) {
-//   return currentScore += rolledNumber();
-// }
-// var win= function(currentScore) {
-//   if (Score >= 100){
-//   return currentScore += rolledNumber()+ "You Win";
-// }
-// }
+//for player 2//
+var rolledNumber2 =function(){
+  return Math.floor((Math.random() * (6)) + 1);
+};
+
+var currentScore2= 0;
+
+
+var roll2= function() {
+  var randomNumber2= rolledNumber2();
+  alert( randomNumber2);
+  if (randomNumber2 === 1){
+    currentScore2 = 0;
+  }else {
+    currentScore2 += randomNumber2;
+  }
+
+}
+
+
+
+var hold1= function(currentScore1) {
+  return currentScore1 += rolledNumber1();
+}
+var hold2= function(currentScore2) {
+  return currentScore2 += rolledNumber2();
+}
+var win= function(currentScore) {
+  if (Score >= 100){
+  return currentScore += rolledNumber()+ "You Win";
+}
+}
 
 Player.prototype.calculation=function(currentScore){
   return currentScore + this.score;
@@ -46,21 +70,26 @@ Player.prototype.calculation=function(currentScore){
 // user interface logic
 $(document).ready(function() {
   $(".btn-roll-player1").click(function() {
-      roll();
+      roll1();
     event.preventDefault();
 
-$("ul#results-player1").text(currentScore);
+$("ul#results-player1").text(currentScore1);
 });
 
   $(".btn-roll-player2").click(function() {
-      roll();
+      roll2();
     event.preventDefault();
 
-  $("ul#results-player2").text(currentScore);
+  $("ul#results-player2").text(currentScore2);
   });
 
 });
 
-  // $("btn-hold-player2").click(function() {
-  //   hold();
-  // $("ul#results-player1").text(currentScore);
+  $(".btn-hold-player1").click(function() {
+    hold1();
+  $("ul#results-player1").text(currentScore1);
+});
+  $(".btn-hold-player2").click(function() {
+    hold2();
+  $("ul#results-player2").text(currentScore1);
+});
